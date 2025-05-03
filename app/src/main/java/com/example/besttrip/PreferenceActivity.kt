7f86e173568655292abb29a534f478499ccde4b8
VAITOcr <kevinjec44@gmail.com>
2025-05-03 15:20:45 -0400

    Fixed AppDatabase and added data in second activity

--- app/src/main/java/com/example/besttrip/PreferenceActivity.kt
+++ app/src/main/java/com/example/besttrip/PreferenceActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.besttrip.baseDeDonees.DestinationDonnees
 
 class PreferenceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preference)
+
+        DestinationDonnees.getDatabase(this)
     }
 }
--- app/src/main/java/com/example/besttrip/baseDeDonees/AppDatabase.kt
+++ app/src/main/java/com/example/besttrip/baseDeDonees/AppDatabase.kt
@@ -1,9 +1,11 @@
 package com.example.besttrip.baseDeDonees
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [Destination::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun destinationDao(): DestinationDAO
